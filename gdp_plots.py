c8a13ab22780637c1674be53ae3f034e834547cc
Sarah Stevens <sarah.stevens@wisc.edu>
2022-01-21 11:11:16 -0600

    Improving plot format

--- gdp_plots.py
+++ gdp_plots.py
@@ -3,12 +3,21 @@ import pandas
 # because we are no longer in a notebook
 import matplotlib.pyplot as plt
 
+filename = 'data/gapminder_gdp_oceania.csv'
+
 # load data and transpose so that country names are
 # the columns and their gdp data becomes the rows
-data = pandas.read_csv('data/gapminder_gdp_oceania.csv', index_col = 'country').T
+data = pandas.read_csv(filename, index_col = 'country').T
 
 # create a plot of the transposed data
-ax = data.plot()
+ax = data.plot(title = filename)
+
+# set some plot attributes
+ax.set_xlabel("Year")
+ax.set_ylabel("GDP Per Capita")
+# set the x location and labels
+ax.set_xticks(range(len(data.index)))
+ax.set_xticklabels(data.index, rotation = 45)
 
 # display the plot
 plt.show()
